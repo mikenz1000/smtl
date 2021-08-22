7fc389dbf54ecdb375e27307077bb001267f0875
Mike Mueller <mikenz@gmail.com>
2021-08-22 21:34:35 +0100

    Make concat support more than two vectors, Use it to clean up qsort concat a bit

--- include/smtl/concat.h
+++ include/smtl/concat.h
@@ -4,13 +4,24 @@
 
 namespace smtl {
 
-template<typename A, typename B>
+template<typename... V>
 struct concat{};
 
-// match when sought at beginning of list
+// concat when there are two vectors
 template<typename T, T... allA, T firstB, T... restB>
 struct concat<vector<allA...>, vector<firstB, restB...>> {
     using value = typename concat<vector<allA..., firstB>, vector<restB...>>::value;
+};
+
+
+// concat when there are more than two vectors
+template<typename T, T... allA, T firstB, T... restB, typename... Rest>
+struct concat<vector<allA...>, vector<firstB, restB...>, Rest...> {
+    using value = typename
+            concat<
+                    typename concat<vector<allA..., firstB>, vector<restB...>>::value,
+                    Rest...
+                    >::value;
 };
 
 // terminator
--- include/smtl/qsort.h
+++ include/smtl/qsort.h
@@ -22,17 +22,14 @@ struct qsort<vector<first>> {
     using value = vector<first>;
 };
 
-
 // general case - choose first element as pivot, split and reassemble as:
 // sorted-left ... pivot ... sorted-right
 template<typename T, T first, T... a>
 struct qsort<vector<first, a...>> {
 
     using value = typename concat<
-            typename concat<
             typename qsort<typename where<vector<a...>, lt<T, first>>::value>::value,
-            vector<first>
-            >::value,
+            vector<first>,
             typename qsort<typename where<vector<a...>, gte<T, first>>::value>::value
             >::value;
 
